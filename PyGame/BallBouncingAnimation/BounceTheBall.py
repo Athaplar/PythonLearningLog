42fa8012fcbd484b30178bf7324afaa2afe568b5
Anil Thaplar <anil.thaplar@gmail.com>
2017-10-08 01:18:11 +0800

    Added bg image, using pygame.display.update instead of pygame.display.flip

--- PyGame/BallBouncingAnimation/BounceTheBall.py
+++ PyGame/BallBouncingAnimation/BounceTheBall.py
@@ -1,27 +1,44 @@
 import pygame
 import sys
+import random
 
 def bounce():
     
     pygame.init()
-    move_offset = [2,2]
+    move_offset = [2,1]
     screen_size = screen_width,screen_height = 900 ,900
     screen = pygame.display.set_mode(screen_size)
+    bg_field_surface = pygame.image.load('field.jpeg')
+    screen.blit(bg_field_surface,(0,0))
     ball_surface = pygame.image.load('ball.png')
     ball_surface_rect = ball_surface.get_rect()
-    
+    pygame.display.update()
+    direction_sing = [1,1]
+	
     while True:
         for event in pygame.event.get():
             if(event.type == pygame.QUIT):
                 pygame.quit()
                 sys.exit()
- 
-        ball_surface_rect = ball_surface_rect.move(move_offset)  
+				
+        screen.blit(bg_field_surface,ball_surface_rect,ball_surface_rect) # erase 
+        ball_surface_rect = ball_surface_rect.move(move_offset)
+        direction_changed = False
+		
         if ball_surface_rect.left < 0 or ball_surface_rect.right > screen_width:
-            move_offset[0] = -move_offset[0]
+            direction_sing[0] = -direction_sing[0]
+            direction_changed = True
+
         if ball_surface_rect.top < 0 or ball_surface_rect.bottom > screen_height:
-            move_offset[1] = -move_offset[1]
+            direction_sing[1] = -direction_sing[1]
+            direction_changed = True
+			
+        if direction_changed:
+            move_offset[0] = random.randint(1,3) * direction_sing[0]
+            move_offset[1] = random.uniform(1,2) * direction_sing [1]
     
-        screen.fill((0,0,0))
+       # screen.fill((0,0,0))
         screen.blit(ball_surface,ball_surface_rect)
-        pygame.display.flip()
+        pygame.time.delay(10)
+        #pygame.display.flip()
+        pygame.display.update()
